32ec68a36d17d3e446c63c4058182a011be8a770
ita27rmp100 <68165849+ita27rmp100@users.noreply.github.com>
2022-11-01 07:38:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,35 @@ class Information extends HTMLElement {
     connectedCallback(){
         let title = this.getAttribute('title')
         let details = this.getAttribute('details')
-        this.innerHTML = '<p><b>'+title+' : </b>'+details+'</p>'
+        this.innerHTML = `<p><b> ${title} : </b>${details}</p>`
     }
 }
 customElements.define('add-info',Information)
+// presentation of my certificate :
+let certificates = [
+    'wordpress',
+    'Websites_Design_with_HTML&CSS',
+    'PBPFF',
+    'front_end_course',
+    'FFS_for_Business',
+    'BPFFS_specialization'
+]
+let num = 0
+function pass(value){
+    if (value == '+') {
+        if (num < certificates.length) {
+            document.getElementById('cert').src = `my_certficate/${certificates[num]}.jpg`
+            num++
+        } else {
+            num = 0
+        }
+    }
+    else {
+        if (num != 0) {
+            document.getElementById('cert').src = `my_certficate/${certificates[num-1]}.jpg`
+            num--
+        } else {
+            num = certificates.length
+        }
+    }
+}
